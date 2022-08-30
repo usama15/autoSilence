488865add194f8db5be5329729e88c849e2cb400
usama shakeel <princeusamaansari15@gmail.com>
2022-08-30 22:56:24 +0500

    init repo

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {NativeBaseProvider, Box, Text} from 'native-base';
+import theme from './lib/theme/Customizetheme';
+import {SafeAreaView} from 'react-native';
+import Splash from './lib/screens/Splash';
+
+const App = () => {
+  return (
+    <NativeBaseProvider theme={theme}>
+      <SafeAreaView>
+        <Splash/>
+      </SafeAreaView>
+    </NativeBaseProvider>
+  );
+};
+
+export default App;
